481ed463b9dedb979b987cfb4e0fbac6e410d005
JIN-415 <jinorabel@gmail.com>
2023-08-16 13:18:15 +0900

    dd

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // const express = require('express');
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //router 분리
 // const userRouter = require('./routes/user');
-import userRouter from './routes/user.js';
-app.use('/user', userRouter);
+import userRouter from "./routes/user.js";
+app.use("/", userRouter);
 //오류처리
-app.use('*', (req, res) => {
-  res.render('404');
+app.use("*", (req, res) => {
+  res.render("404");
 });
 
 app.listen(PORT, () => {
